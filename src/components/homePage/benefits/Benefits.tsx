c64cb40fed1aadbbf10a90eb0db74a577709aa9e
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-28 20:29:58 +0200

    added button

--- src/components/homePage/benefits/Benefits.tsx
+++ src/components/homePage/benefits/Benefits.tsx
@@ -3,6 +3,7 @@ import SectionTitle from "@/components/shared/footer/titles/SectionTitle";
 import React from "react";
 import { useTranslations } from "next-intl";
 import BenefitsList from "./BenefitsList";
+import Button from "@/components/shared/buttons/Button";
 
 export default function Benefits() {
   const t = useTranslations();
@@ -13,6 +14,9 @@ export default function Benefits() {
         {t("homePage.benefits.title")}
       </SectionTitle>
       <BenefitsList />
+      <Button className="mx-auto mt-5 laptop:mt-[60px]">
+        {t("buttons.makeOrder")}
+      </Button>
     </Section>
   );
 }
--- src/components/shared/buttons/Button.tsx
+++ src/components/shared/buttons/Button.tsx
@@ -18,8 +18,9 @@ export default function Button({
       type={type}
       disabled={disabled}
       onClick={onClick}
-      className={`flex items-center justify-center h-[54px] laptop:h-[85px] px-3 text-14bold laptop:text-24bold rounded-full transition duration-300 ease-out 
-        enabled:active:brightness-[115%] enabled:active:scale-95 laptop:enabled:hover:brightness-[115%] enabled:focus-visible:brightness-[115%] outline-none ${
+      className={`flex items-center justify-center w-[350px] deskxl:w-[437px] h-[54px] laptop:h-[82px] desk:h-[85px] px-3 text-14bold laptop:text-18bold 
+        deskxl:text-24bold rounded-full transition duration-300 ease-out enabled:active:brightness-[115%] enabled:active:scale-95 
+        laptop:enabled:hover:brightness-[115%] enabled:focus-visible:brightness-[115%] outline-none ${
           variant === "primary" ? "bg-yellowGradient" : "bg-lightGrey"
         }  
         
