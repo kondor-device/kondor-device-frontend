2e14ca718484807900e129b557fdc8c4834e21c8
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-28 20:02:00 +0200

    added benefits list

--- src/components/homePage/benefits/Benefits.tsx
+++ src/components/homePage/benefits/Benefits.tsx
@@ -2,15 +2,17 @@ import Section from "@/components/shared/section/Section";
 import SectionTitle from "@/components/shared/footer/titles/SectionTitle";
 import React from "react";
 import { useTranslations } from "next-intl";
+import BenefitsList from "./BenefitsList";
 
 export default function Benefits() {
   const t = useTranslations();
 
   return (
-    <Section>
+    <Section className="pb-[60px] laptop:pb-[100px]">
       <SectionTitle className="text-center">
         {t("homePage.benefits.title")}
       </SectionTitle>
+      <BenefitsList />
     </Section>
   );
 }
