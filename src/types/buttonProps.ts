5ec7e24fd5dc947b5274bf0a67d8c775ecf74555
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-28 20:15:22 +0200

    added button component

---
+++ src/types/buttonProps.ts
@@ -0,0 +1,12 @@
+import { Dispatch, SetStateAction } from "react";
+
+export interface ButtonProps {
+  children: string | React.JSX.Element;
+  ariaLabel?: string;
+  className?: string;
+  type?: "submit" | "button";
+  variant?: "primary" | "secondary";
+  disabled?: boolean;
+  isLoading?: boolean;
+  onClick?: () => void | Dispatch<SetStateAction<boolean>>;
+}
