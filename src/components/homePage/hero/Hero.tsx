a84407049db7db6975ea409871a83dd662362385
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-29 22:26:13 +0200

    added go to catalog button

--- src/components/homePage/hero/Hero.tsx
+++ src/components/homePage/hero/Hero.tsx
@@ -1,11 +1,23 @@
 import Section from "@/components/shared/section/Section";
 import React from "react";
-import HeroInfo from "./HeroInfo";
+import HeroTitle from "./HeroTitle";
+import Button from "@/components/shared/buttons/Button";
+import { Link } from "@/i18n/routing";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function Hero() {
+  const locale = useLocale();
+  const t = useTranslations();
+
   return (
     <Section>
-      <HeroInfo />
+      <HeroTitle />
+      <Link
+        href={locale === "uk" ? `/#catalog` : `/${locale}#catalog`}
+        className="block w-fit mt-[30px] laptop:mt-[42px] mx-auto laptop:mx-0"
+      >
+        <Button>{t("buttons.goToCatalog")}</Button>
+      </Link>
     </Section>
   );
 }
