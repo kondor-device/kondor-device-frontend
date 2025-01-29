63d52596139ae1b04ab7d1aa1450fe197ece1117
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-29 14:52:46 +0200

    added warranty info

--- src/components/warrantyPage/Warranty.tsx
+++ src/components/warrantyPage/Warranty.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import PageTitle from "../shared/titles/PageTitle";
 import { useTranslations } from "next-intl";
+import WarrantyInfo from "./WarrantyInfo";
+import Button from "../shared/buttons/Button";
 
 export default function Warranty() {
-  const t = useTranslations("warrantyPage");
+  const t = useTranslations("");
 
   return (
-    <div>
-      <PageTitle>{t("title")}</PageTitle>
-    </div>
+    <>
+      <PageTitle>{t("warrantyPage.title")}</PageTitle>
+      <section className="container max-w-[1920px] pb-5 laptop:pb-[100px]">
+        <WarrantyInfo />
+        <Button>{t("buttons.haveQuestions")}</Button>
+      </section>
+    </>
   );
 }
