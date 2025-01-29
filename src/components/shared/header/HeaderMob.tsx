e96f9b757395a0602b7b650d9a916347336cafb4
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-29 18:37:08 +0200

    added header mob

--- src/components/shared/header/HeaderMob.tsx
+++ src/components/shared/header/HeaderMob.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import LogoLink from "@/components/shared/logoLink/LogoLink";
+import BurgerMenu from "./BurgerMenu";
+import BurgerMenuButton from "./BurgerMenuButton";
 
 export default function HeaderMob() {
-  return <div className="laptop:hidden">HeaderMob</div>;
+  const [isHeaderMenuOpened, setIsHeaderMenuOpened] = useState(false);
+  const toggleHeaderMenuOpen = () => setIsHeaderMenuOpened(!isHeaderMenuOpened);
+
+  return (
+    <div className={`laptop:hidden fixed z-10 w-full h-[82px] bg-white`}>
+      <div className="container flex items-center justify-between max-w-[1920px] h-full">
+        <LogoLink className="w-[152px]" />
+        <BurgerMenuButton
+          isHeaderMenuOpened={isHeaderMenuOpened}
+          toggleHeaderMenuOpen={toggleHeaderMenuOpen}
+        />
+      </div>
+      <BurgerMenu
+        isHeaderMenuOpened={isHeaderMenuOpened}
+        setIsHeaderMenuOpened={setIsHeaderMenuOpened}
+      />
+    </div>
+  );
 }
