eef2ba6809017c45002c919b89ee21bff20d5f81
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-29 17:39:19 +0200

    added not found page

---
+++ src/components/notFoundPage/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PageTitle from "../shared/titles/PageTitle";
+import Button from "../shared/buttons/Button";
+import { useLocale, useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
+
+export default function NotFoundPage() {
+  const t = useTranslations("");
+  const locale = useLocale();
+
+  return (
+    <>
+      <PageTitle>{t("notFoundPage.info")}</PageTitle>
+      <section className="container max-w-[1920px] py-[100px]">
+        <Link href="/" locale={locale} className="block w-fit mx-auto">
+          <Button>{t("buttons.goHome")}</Button>
+        </Link>
+      </section>
+    </>
+  );
+}
