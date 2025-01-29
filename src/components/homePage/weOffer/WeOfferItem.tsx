b77b1c8283dfef41ff85a082d85d9d33ac35d99a
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-29 20:28:08 +0200

    updated we offer styles for tablet

--- src/components/homePage/weOffer/WeOfferItem.tsx
+++ src/components/homePage/weOffer/WeOfferItem.tsx
@@ -12,7 +12,7 @@ export default function WeOfferItem({ weOfferItem }: WeOfferItemProps) {
   const { title, icon } = weOfferItem;
 
   return (
-    <li className="flex justify-center items-center w-full px-[30px] deskxl:px-[39px] py-5 laptop:py-[30px] rounded-[20px] bg-yellowGradient">
+    <li className="flex items-center w-full max-w-[545px] px-[30px] deskxl:px-[39px] py-5 laptop:py-[30px] rounded-[20px] bg-yellowGradient">
       <Image
         src={`/images/icons/${icon}Black.svg`}
         alt={title}
@@ -20,9 +20,9 @@ export default function WeOfferItem({ weOfferItem }: WeOfferItemProps) {
         height="46"
         className="mr-[15px] laptop:mr-5"
       />
-      <span className="w-[198px] laptop:w-[229px] deskxl:w-[299px] text-14semi laptop:text-16semi deskxl:text-20semi">
+      <p className="text-14semi laptop:text-16semi deskxl:text-20semi">
         {title}
-      </span>
+      </p>
     </li>
   );
 }
