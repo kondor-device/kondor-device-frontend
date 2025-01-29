c20ec53669a3f2d590fbd8e56ce7b6ae58003208
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-29 17:06:09 +0200

    added about page join us button

--- src/components/aboutPage/About.tsx
+++ src/components/aboutPage/About.tsx
@@ -3,7 +3,7 @@ import PageTitle from "../shared/titles/PageTitle";
 import { useTranslations } from "next-intl";
 
 import Button from "../shared/buttons/Button";
-import { TELEGRAM_URL } from "@/constants/constants";
+import { INSTAGRAM_URL } from "@/constants/constants";
 import AboutInfo from "./AboutInfo";
 
 export default function About() {
@@ -12,21 +12,14 @@ export default function About() {
     <>
       <PageTitle>{t("aboutPage.title")}</PageTitle>
       <section className="container max-w-[1920px] py-5 laptop:py-[100px]">
+        <AboutInfo />
         <a
-          href={TELEGRAM_URL}
+          href={INSTAGRAM_URL}
           target="_blank"
           rel="noopener noreferrer nofollow"
           className="block w-fit mx-auto laptop:mx-0"
         >
-          <AboutInfo />
-          <Button>
-            <p>
-              {t("buttons.haveQuestions")}
-              <span className="text-14med laptop:text-18reg">
-                {t("buttons.telegram")}
-              </span>
-            </p>
-          </Button>
+          <Button>{t("buttons.join")}</Button>
         </a>
       </section>
     </>
