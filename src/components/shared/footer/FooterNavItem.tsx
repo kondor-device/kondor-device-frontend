fe56085c55083fc7b9a4ec79904c505563cbffff
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-29 23:34:05 +0200

    updated leading styles

--- src/components/shared/footer/FooterNavItem.tsx
+++ src/components/shared/footer/FooterNavItem.tsx
@@ -14,7 +14,7 @@ export default function FooterNavItem({ navItem }: FooterNavItemProps) {
     <li className="leading-none">
       <Link
         href={path}
-        className="text-12med laptop:text-18med leading-[22px] laptop:hover:text-yellow focus-visible:text-yellow active:text-yellow transition duration-300 ease-out"
+        className="text-12med laptop:text-18med laptop:hover:text-yellow focus-visible:text-yellow active:text-yellow transition duration-300 ease-out"
       >
         {title}
       </Link>
