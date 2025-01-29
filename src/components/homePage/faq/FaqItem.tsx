ed6da9c6f678c04ffcbb833976b1adbb019c4fb1
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-29 12:16:47 +0200

    added faq item functionality

--- src/components/homePage/faq/FaqItem.tsx
+++ src/components/homePage/faq/FaqItem.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 
 interface FaqItemProps {
@@ -9,10 +10,16 @@ interface FaqItemProps {
 }
 
 export default function FaqItem({ faqItem }: FaqItemProps) {
+  const [isShownMore, setIsShownMore] = useState(false);
+  const toggleShowMore = () => setIsShownMore(!isShownMore);
+
   const { title, description } = faqItem;
 
   return (
-    <li className="px-[20px] py-[15px] laptop:px-[50px] laptop:py-[40px] rounded-[11px] laptop:rounded-[30px] shadow-card leading-none">
+    <li
+      onClick={toggleShowMore}
+      className="cursor-pointer px-[20px] py-[15px] laptop:px-[50px] laptop:py-[40px] rounded-[11px] laptop:rounded-[30px] shadow-card leading-none"
+    >
       <div className="flex items-center mb-[5px] laptop:mb-4">
         <Image
           src={`/images/icons/question.svg`}
@@ -29,10 +36,16 @@ export default function FaqItem({ faqItem }: FaqItemProps) {
           alt="cross icon"
           width="32"
           height="33"
-          className="w-[10px] laptop:w-6 h-auto ml-auto"
+          className={`w-[10px] laptop:w-6 h-auto ml-auto transition duration-700 ease-in-out ${
+            isShownMore ? "rotate-45" : "rotate-0"
+          }`}
         />
       </div>
-      <p className="pl-8 laptop:pl-[64px] text-12med laptop:text-18med">
+      <p
+        className={`pl-8 laptop:pl-[64px] text-12med laptop:text-18med overflow-hidden transition-[max-height] duration-700 ease-in-out ${
+          isShownMore ? "max-h-[600px]" : "max-h-0"
+        }`}
+      >
         {description}
       </p>
     </li>
