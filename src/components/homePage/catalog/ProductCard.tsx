a5298a8e10abbc22b9ec7f54fbd2305cb911e63c
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-30 22:04:07 +0200

    updated product card

--- src/components/homePage/catalog/ProductCard.tsx
+++ src/components/homePage/catalog/ProductCard.tsx
@@ -3,6 +3,7 @@ import { ProductItem } from "@/types/productItem";
 import Image from "next/image";
 import React from "react";
 import { useTranslations } from "next-intl";
+import SecondaryButton from "@/components/shared/buttons/SecondaryButton";
 
 interface ProductCardProps {
   product: ProductItem;
@@ -11,6 +12,9 @@ interface ProductCardProps {
 export default function ProductCard({ product }: ProductCardProps) {
   const t = useTranslations();
   const { generalname, name, price, priceDiscount, coloropts } = product;
+
+  const savings = (((price - priceDiscount) / price) * 100).toFixed(2);
+
   console.log(product);
 
   return (
@@ -26,13 +30,19 @@ export default function ProductCard({ product }: ProductCardProps) {
         <p className="text-white">{generalname}</p>
         <p className="text-yellow">{name}</p>
       </h3>
+      <span className="size-[18px] laptop:size-[45px] rounded-full"></span>
       <span className="text-18med laptop:text-54bold text-white">
         {priceDiscount}
+        {t("homePage.catalog.hrn")}
       </span>
       <span className="text-14bold laptop:text-22bold text-grey line-through">
         {price}
       </span>
-      <span className="text-10med laptop:text-16med text-yellow"></span>
+      <span className="text-10med laptop:text-16med text-yellow">
+        {savings}%
+      </span>
+      <SecondaryButton>{t("homePage.catalog.characteristics")}</SecondaryButton>
+      <SecondaryButton>{t("homePage.catalog.set")}</SecondaryButton>
       <Button>{t("buttons.makeOrder")}</Button>
     </div>
   );
