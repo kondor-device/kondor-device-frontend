868490a7556a9f8d4c55d0477f53cfa49232b1ea
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-01 20:12:56 +0200

    updated modal styles

--- src/components/shared/modal/Modal.tsx
+++ src/components/shared/modal/Modal.tsx
@@ -36,8 +36,8 @@ export default function Modal({
       className={`${
         isPopUpShown
           ? " -translate-y-[calc(50dvh-50%)] opacity-100"
-          : "pointer-events-none opacity-0"
-      } fixed z-50 left-1/2 bottom-0 transform -translate-x-1/2 transition duration-[600ms] ease-out min-w-[312px] max-w-[368px] tab:max-w-[496px] laptop:tab:max-w-[950px]
+          : "translate-y-1/2 pointer-events-none opacity-0"
+      } fixed z-50 left-1/2 bottom-0 transform -translate-x-1/2 transition duration-700 ease-out min-w-[312px] max-w-[368px] tab:max-w-[496px] laptop:tab:max-w-[950px]
       w-[86.6%] tab:w-[496px] laptop:w-[950px] max-h-[90dvh] overflow-y-auto px-5 py-[30px] laptop:px-[75px] laptop:py-14 rounded-[20px] laptop:rounded-[30px] bg-white
        scrollbar scrollbar-w-[3px] laptop:scrollbar-w-[8px] scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-yellow scrollbar-track-transparent popup-scroll`}
     >
