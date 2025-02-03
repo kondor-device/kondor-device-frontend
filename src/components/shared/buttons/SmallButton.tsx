ce74365a67ceb22b3b5f4e10540537c7054cd511
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-04 00:06:13 +0200

    updated small button active styles

--- src/components/shared/buttons/SmallButton.tsx
+++ src/components/shared/buttons/SmallButton.tsx
@@ -14,7 +14,7 @@ export default function SmallButton({
       type={type}
       onClick={onClick}
       className={`flex items-center justify-center w-[122px] tab:w-[182px] deskxl:w-[278px] px-[10px] py-[5px] deskxl:py-[10px] deskxl:px-[25px] text-12bold tab:text-20bold deskxl:text-28bold rounded-full bg-white 
-        border laptop:border-[3px] border-dark transition duration-300 ease-out enabled:active:scale-95 laptop:enabled:hover:bg-yellow laptop:enabled:hover:border-yellow 
+        border laptop:border-[3px] border-dark transition duration-300 ease-out enabled:active:scale-95 enabled:active:border-yellow enabled:active:bg-yellow laptop:enabled:hover:bg-yellow laptop:enabled:hover:border-yellow 
         enabled:focus-visible:border-yellow enabled:focus-visible:bg-yellow outline-none ${className}`}
     >
       {children}
