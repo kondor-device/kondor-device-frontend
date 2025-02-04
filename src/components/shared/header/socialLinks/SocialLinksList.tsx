e6070ca9eb170932d1113e7319bb039eceb3a54e
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-04 15:24:08 +0200

    added closing burger menu on social links click

--- src/components/shared/header/socialLinks/SocialLinksList.tsx
+++ src/components/shared/header/socialLinks/SocialLinksList.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import SocialLinkItem from "./SocialLinkItem";
 import { INSTAGRAM_URL, TELEGRAM_URL } from "@/constants/constants";
 
 interface SocialLinksListProps {
   className?: string;
+  setIsHeaderMenuOpened?: Dispatch<SetStateAction<boolean>>;
 }
 
 export default function SocialLinksList({
   className = "",
+  setIsHeaderMenuOpened,
 }: SocialLinksListProps) {
   const socialLinksList = [
     {
@@ -23,7 +25,11 @@ export default function SocialLinksList({
   return (
     <ul className={`flex items-center gap-8 ${className}`}>
       {socialLinksList.map((social, idx) => (
-        <SocialLinkItem key={idx} social={social} />
+        <SocialLinkItem
+          key={idx}
+          social={social}
+          setIsHeaderMenuOpened={setIsHeaderMenuOpened}
+        />
       ))}
     </ul>
   );
