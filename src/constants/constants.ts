f53f069144f87fa3e4e776ed977898846c3d59bb
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-04 18:23:38 +0200

    added phone mask

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,26 @@
 export const EMAIL_FIRST = "support@kondordevice.com";
 export const EMAIL_SECOND = "partner@kondordevice.com";
+
 export const TELEGRAM = "@kondor_device";
 export const INSTAGRAM = "@kondor.ua";
+
 export const TELEGRAM_URL = "https://t.me/kondor_device";
 export const INSTAGRAM_URL = "https://www.instagram.com/kondor.ua";
+
 export const DATOCMS_URL = "https://graphql.datocms.com/";
+
+export const PHONE_NUMBER_MASK = [
+  /\d/,
+  /\d/,
+  /\d/,
+  " ",
+  /\d/,
+  /\d/,
+  /\d/,
+  "-",
+  /\d/,
+  /\d/,
+  "-",
+  /\d/,
+  /\d/,
+];
