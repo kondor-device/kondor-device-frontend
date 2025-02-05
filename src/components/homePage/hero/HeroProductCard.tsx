3c51ab26d0fa9e2a31b58425e7dd92eb7fe1c625
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-05 11:28:45 +0200

    updated hero product card

--- src/components/homePage/hero/HeroProductCard.tsx
+++ src/components/homePage/hero/HeroProductCard.tsx
@@ -22,13 +22,13 @@ export default function HeroProductCard({ product }: HeroProductCardProps) {
       className="flex flex-col justify-between w-[48.5%] deskxl:w-[48%] max-w-[350px] rounded-[18px] laptop:rounded-[24px] deskxl:rounded-[40px] p-[18px] laptop:p-[22px] deskxl:py-[30px] deskxl:px-10 
     shadow-card bg-white"
     >
-      <div className="flex items-center justify-center w-full aspect-[1/1]">
+      <div className="flex items-center justify-center w-full tab:w-[85%] laptop:w-[65%] deskxl:size-[52.3%] aspect-[1/1] mx-auto my-auto">
         <Image
           src={photos[0].url}
           alt={photos[0].alt || "keyboard"}
           width={1080}
           height={1080}
-          className="w-[100%] tab:w-[85%] laptop:w-[65%] deskxl:size-[52.3%] mx-auto my-auto"
+          className="w-full h-auto"
         />
       </div>
       <h3 className="mt-auto mb-[11px] desk:mb-[15px] deskxl:mb-[25px] text-14bold sm:text-20bold deskxl:text-32bold text-center">
