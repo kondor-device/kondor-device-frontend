41f5c58f9193e4f9fc07b8ffa9c2f9bc7c057e99
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-05 10:56:53 +0200

    updated main button styles

--- src/components/shared/buttons/Button.tsx
+++ src/components/shared/buttons/Button.tsx
@@ -21,7 +21,7 @@ export default function Button({
       className={`flex items-center justify-center min-h-[36px] laptop:min-h-[64px] deskxl:min-h-[85px] px-8 laptop:px-[50px] py-[19px] laptop:py-[30px] text-14bold laptop:text-18bold 
         deskxl:text-24bold  rounded-full transition duration-300 ease-out enabled:active:scale-95 outline-none ${
           variant === "primary"
-            ? "bg-yellowGradient border-[2px] border-yellow enabled:active:bg-darkGradient laptop:enabled:hover:bg-darkGradient enabled:focus-visible:bg-darkGradient laptop:enabled:hover:text-yellow enabled:focus-visible:text-yellow enabled:active:text-yellow"
+            ? "bg-yellowGradient enabled:active:brightness-[115%] laptop:enabled:hover:brightness-[115%] enabled:focus-visible:brightness-[115%]"
             : "bg-lightGrey enabled:active:bg-grey laptop:enabled:hover:bg-grey enabled:focus-visible:bg-grey"
         }  
         
