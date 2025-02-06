13504e8dd94e02664151729e34087db255781e3e
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-06 17:40:54 +0200

    added search warehouses

---
+++ src/utils/searchWarehouses.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export async function searchWarehouses(cityRef: string, query: string) {
+  if (query.length < 2) return [];
+
+  try {
+    const { data } = await axios.post("/api/novaposhta/warehouses", {
+      cityRef,
+      query,
+    });
+    return data;
+  } catch (error) {
+    console.error("Error fetching warehouses:", error);
+    return [];
+  }
+}
