ff538ce4e438b558a0ac43412760a6e741e9ba0f
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-06 17:40:03 +0200

    added search cities

---
+++ src/utils/searchCities.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export async function searchCities(query: string) {
+  if (query.length < 2) return [];
+
+  try {
+    const { data } = await axios.post("/api/novaposhta/cities", { query });
+    return data;
+  } catch (error) {
+    console.error("Error fetching cities:", error);
+    return [];
+  }
+}
