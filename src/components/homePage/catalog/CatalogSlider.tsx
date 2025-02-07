e3861f44c5d56b10fa3426b974aa42208914a299
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-07 15:24:39 +0200

    updated for > 2 slides

--- src/components/homePage/catalog/CatalogSlider.tsx
+++ src/components/homePage/catalog/CatalogSlider.tsx
@@ -38,7 +38,7 @@ export default function CatalogSlider({
       <h2 className="container w-full max-w-[1920px] mb-[30px] text-22bold laptop:text-40bold text-center">
         {title}
       </h2>
-      {products.length > 1 ? (
+      {products.length > 2 ? (
         <Swiper
           centeredSlides={true}
           slidesPerView="auto"
