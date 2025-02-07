3ebbda4b87415a411cac70004a69ce967440dd3e
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-07 20:26:59 +0200

    updated payment function

--- src/utils/handleSubmitForm.ts
+++ src/utils/handleSubmitForm.ts
@@ -1,3 +1,21 @@
+declare global {
+  interface WayforpayData {
+    orderReference: string;
+    orderDate: number;
+    amount: number;
+    currency: string;
+    productName: string[];
+    productPrice: number[];
+    productCount: number[];
+  }
+
+  interface Window {
+    Wayforpay: {
+      run: (data: WayforpayData) => void;
+    };
+  }
+}
+
 import { ValuesCheckoutFormType } from "@/components/homePage/catalog/checkout/CheckoutPopUp";
 import axios from "axios";
 import { FormikHelpers } from "formik";
@@ -16,62 +34,94 @@ export const handleSubmitForm = async <T>(
   setIsNotificationShown: Dispatch<SetStateAction<boolean>>,
   values: ValuesCheckoutFormType
 ) => {
+  const { clearOrderData, setOrderData, orderData } = useOrderStore.getState();
+  const { clearCart, cartItems, getTotalAmount } = useCartStore.getState();
+
+  clearOrderData();
+
+  const orderNumber = generateOrderNumber();
+  const totalSum = getTotalAmount();
+  const orderedListProducts = cartItems
+    .map(
+      (cartItem) =>
+        `- ${cartItem.generalName} ${cartItem.name}, колір: ${cartItem.color}`
+    )
+    .join("\n");
+
+  const collectedOrderData = {
+    orderNumber,
+    name: values.name.trim(),
+    surname: values.surname.trim(),
+    phone: values.phone.trim(),
+    city: values.city.trim(),
+    postOffice: values.postOffice.trim(),
+    promocode: values.promocode.trim(),
+    payment: values.payment.trim(),
+    cartItems,
+    totalSum,
+  };
+
+  const dataTelegram =
+    `<b>Замовлення #${orderNumber}</b>\n` +
+    `<b>Ім'я:</b> ${values.name.trim()}\n` +
+    `<b>Прізвище:</b> ${values.surname.trim()}\n` +
+    `<b>Телефон:</b> ${values.phone.replace(/[^\d+]/g, "")}\n` +
+    `<b>Насeлений пункт:</b> ${values.city.trim()}\n` +
+    `<b>Відділення Нової пошти:</b> ${values.postOffice.trim() || ""}\n` +
+    `<b>Промокод:</b> ${values.promocode?.trim()}\n` +
+    `<b>Оплата:</b> ${values.payment.trim()}\n` +
+    `<b>Список товарів:</b>\n ${orderedListProducts}\n` +
+    `<b>Сума замовлення:</b> ${totalSum} грн\n`;
+
+  const dataGoogle = {
+    orderNumber,
+    name: values.name.trim(),
+    surname: values.surname.trim(),
+    phone: values.phone.replace(/[^\d+]/g, ""),
+    city: values.city.trim(),
+    postOffice: values.postOffice.trim(),
+    promocode: values.promocode.trim(),
+    payment: values.payment.trim(),
+    orderedListProducts,
+    totalSum: `${totalSum} грн`,
+  };
+
+  setOrderData(collectedOrderData);
+
+  const productName = cartItems.map(
+    (item) => `${item.name} ${item.name}, колір: ${item.color}`
+  );
+  const productPrice = cartItems.map(
+    (item) => item.priceDiscount ?? item.price
+  );
+  const productCount = cartItems.map(() => 1);
+
+  if (values.payment.trim() === "Онлайн оплата (Wayforpay)") {
+    console.log("старт оплати");
+    try {
+      const { data } = await axios.post(`${BASE_URL}api/wayforpay/invoice`, {
+        orderReference: `#${orderNumber}`,
+        orderDate: Math.floor(Date.now() / 1000),
+        amount: totalSum,
+        currency: "UAH",
+        productName,
+        productPrice,
+        productCount,
+      });
+
+      if (data?.status === "success") {
+        // Викликаємо платіжну форму WayForPay
+        window.Wayforpay.run(data.paymentData);
+      } else {
+        console.error("Помилка створення платежу:", data.error);
+      }
+    } catch (error) {
+      console.error("Помилка запиту на оплату:", error);
+    }
+  }
+
   try {
     setIsLoading(true);
-
-    const { clearOrderData, setOrderData } = useOrderStore.getState();
-    const { clearCart, cartItems, getTotalAmount } = useCartStore.getState();
-
-    clearOrderData();
-
-    const orderNumber = generateOrderNumber();
-    const totalSum = getTotalAmount();
-    const orderedListProducts = cartItems
-      .map(
-        (cartItem) =>
-          `- ${cartItem.generalName} ${cartItem.name}, колір: ${cartItem.color}`
-      )
-      .join("\n");
-
-    const orderData = {
-      orderNumber,
-      name: values.name.trim(),
-      surname: values.surname.trim(),
-      phone: values.phone.trim(),
-      city: values.city.trim(),
-      postOffice: values.postOffice.trim(),
-      promocode: values.promocode.trim(),
-      payment: values.payment.trim(),
-      cartItems,
-      totalSum,
-    };
-
-    setOrderData(orderData);
-
-    const dataTelegram =
-      `<b>Замовлення #${orderNumber}</b>\n` +
-      `<b>Ім'я:</b> ${values.name.trim()}\n` +
-      `<b>Прізвище:</b> ${values.surname.trim()}\n` +
-      `<b>Телефон:</b> ${values.phone.replace(/[^\d+]/g, "")}\n` +
-      `<b>Насeлений пункт:</b> ${values.city.trim()}\n` +
-      `<b>Відділення Нової пошти:</b> ${values.postOffice.trim() || ""}\n` +
-      `<b>Промокод:</b> ${values.promocode?.trim()}\n` +
-      `<b>Оплата:</b> ${values.payment.trim()}\n` +
-      `<b>Список товарів:</b>\n ${orderedListProducts}\n` +
-      `<b>Сума замовлення:</b> ${totalSum} грн\n`;
-
-    const dataGoogle = {
-      orderNumber,
-      name: values.name.trim(),
-      surname: values.surname.trim(),
-      phone: values.phone.replace(/[^\d+]/g, ""),
-      city: values.city.trim(),
-      postOffice: values.postOffice.trim(),
-      promocode: values.promocode.trim(),
-      payment: values.payment.trim(),
-      orderedListProducts,
-      totalSum: `${totalSum} грн`,
-    };
 
     await axios({
       method: "post",
