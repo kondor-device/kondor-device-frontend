721e1ef06afbadfdb9e74440aa612dea9287b368
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-08 20:11:25 +0200

    updated merchant domain

--- src/app/api/wayforpay/invoice/route.ts
+++ src/app/api/wayforpay/invoice/route.ts
@@ -3,7 +3,7 @@ import crypto from "crypto";
 
 const MERCHANT_ACCOUNT = process.env.MERCHANT_ACCOUNT;
 const MERCHANT_SECRET_KEY = process.env.MERCHANT_SECRET_KEY;
-const MERCHANT_DOMAIN = "merchant.com.ua";
+const MERCHANT_DOMAIN = process.env.NEXT_PUBLIC_BAE_URL;
 
 export async function POST(req: NextRequest) {
   if (!MERCHANT_SECRET_KEY) {
@@ -22,7 +22,7 @@ export async function POST(req: NextRequest) {
       productCount,
     } = body;
 
-    // Формуємо рядок для підпису (СТРОГИЙ ПОРЯДОК!)
+    // Формуємо рядок для підпису
     const signString = [
       MERCHANT_ACCOUNT,
       MERCHANT_DOMAIN,
@@ -40,7 +40,7 @@ export async function POST(req: NextRequest) {
     const merchantSignature = hmac.digest("hex");
 
     const paymentData = {
-      transactionType: "CREATE_INVOICE", // Вказуємо тип операції
+      transactionType: "CREATE_INVOICE",
       merchantAccount: MERCHANT_ACCOUNT,
       merchantDomainName: MERCHANT_DOMAIN,
       merchantAuthType: "SimpleSignature",
