3189ae52dc84fe830ab5ee76bdb7dc0d70e69462
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-08 21:07:43 +0200

    added shadow to the modals

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,7 +82,7 @@ export default {
         colorPicker: "0 0 0 2px #FFFFFF",
         imagePicker: "0 0 0 2px #FFB300",
         radio: "0 0 0 1px #FFB300",
-        notification: "0px 66px 150px 0px rgba(25, 25, 25, 0.3)",
+        notification: "0px 4px 100px 0px rgba(25, 25, 25, 0.6)",
       },
       keyframes: {
         rotate: {
