145be44c7cac2ea2a03b501b627fe431920adbe5
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-11 16:32:49 +0200

    added animation to the we offer section

--- src/components/homePage/weOffer/WeOffer.tsx
+++ src/components/homePage/weOffer/WeOffer.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import WeOfferList from "./WeOfferList";
 
+const SECTION_ID = "home-page-we-offer";
+
 export default function WeOffer() {
   return (
-    <section className="container w-full max-w-[1920px] laptop:pt-[50px]">
+    <section
+      id={SECTION_ID}
+      className="container w-full max-w-[1920px] laptop:pt-[50px]"
+    >
       <WeOfferList />
     </section>
   );
