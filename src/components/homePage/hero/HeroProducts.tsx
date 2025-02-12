7df63d6ee9c50c0851524fd984774b0721045c80
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-12 18:21:31 +0200

    updated hero products card styles

--- src/components/homePage/hero/HeroProducts.tsx
+++ src/components/homePage/hero/HeroProducts.tsx
@@ -18,7 +18,7 @@ export default async function HeroProducts({
       : shownOnMainProducts;
 
   return (
-    <ul className="flex flex-wrap justify-between gap-y-[14px] laptop:gap-[16px] deskxl:gap-6 laptop:w-[49%] desk:w-[42%] deskxl:w-[54.5%] max-w-[704px] laptop:max-w-full my-[30px] laptop:my-auto mx-auto deskxl:mr-0">
+    <ul className="flex flex-wrap justify-center gap-2 mob:gap-[10px] sm:gap-[16px] deskxl:gap-6 laptop:w-[49%] desk:w-[42%] deskxl:w-[54.5%] max-w-[704px] laptop:max-w-full my-[30px] laptop:my-auto mx-auto deskxl:mr-0">
       {heroProducts.map((product: ProductItem) => (
         <HeroProductCard key={product.id} product={product} />
       ))}
