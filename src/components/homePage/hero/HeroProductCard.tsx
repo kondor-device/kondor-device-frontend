26b530ea7aaf28c172c09b644c292f88b71075a4
Olga Mykhailova <rosebud_z@yahoo.com>
2025-02-12 20:05:04 +0200

    updated hero styles for tab xl

--- src/components/homePage/hero/HeroProductCard.tsx
+++ src/components/homePage/hero/HeroProductCard.tsx
@@ -24,10 +24,10 @@ export default function HeroProductCard({ product }: HeroProductCardProps) {
 
   return (
     <li
-      className="flex flex-col justify-between w-[48.5%] deskxl:w-[48%] max-w-[350px] rounded-[18px] laptop:rounded-[24px] deskxl:rounded-[40px] p-[18px] laptop:p-[22px] deskxl:py-[30px] deskxl:px-10 
+      className="flex flex-col justify-between w-[48.5%] tabxl:w-[47%] laptop:w-[48.5%] deskxl:w-[48%] max-w-[350px] rounded-[18px] tabxl:rounded-[24px] deskxl:rounded-[40px] p-[18px] tabxl:p-[22px] deskxl:py-[30px] deskxl:px-10 
     shadow-card bg-white"
     >
-      <div className="flex items-center justify-center w-full tab:w-[85%] laptop:w-[65%] deskxl:size-[90%] aspect-[1/1] mx-auto my-auto">
+      <div className="flex items-center justify-center w-full tab:w-[85%] tabxl:w-[65%] deskxl:size-[90%] aspect-[1/1] mx-auto my-auto">
         <Link
           href={cat?.name ? localizedCategoryLink : localizedCatalogLink}
           className="group block w-fit mx-auto"
@@ -38,11 +38,11 @@ export default function HeroProductCard({ product }: HeroProductCardProps) {
             width={1080}
             height={1080}
             priority
-            className="w-full h-auto scale-100 laptop:group-hover:scale-105 transition duration-1000 ease-out"
+            className="w-full h-auto scale-100 tabxl:group-hover:scale-105 transition duration-1000 ease-out"
           />
         </Link>
       </div>
-      <h2 className="mt-auto mb-[11px] desk:mb-[15px] deskxl:mb-[25px] text-12bold mob:text-14bold sm:text-20bold deskxl:text-28bold text-center">
+      <h2 className="mt-auto mb-[11px] desk:mb-[15px] deskxl:mb-[25px] text-12bold mob:text-14bold sm:text-20bold tabxl:text-18bold laptop:text-20bold deskxl:text-28bold text-center">
         {name}
       </h2>
       <Link
