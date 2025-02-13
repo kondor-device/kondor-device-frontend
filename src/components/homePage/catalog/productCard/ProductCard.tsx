4485a6eb46433c4e20c07a31b1de249f3c899684
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-02-13 22:15:47 +0200

    added 'add_to_cart' event

--- src/components/homePage/catalog/productCard/ProductCard.tsx
+++ src/components/homePage/catalog/productCard/ProductCard.tsx
@@ -14,6 +14,7 @@ import { v4 as uuidv4 } from "uuid";
 import { useModalStore } from "@/store/modalStore";
 import SecondaryButton from "@/components/shared/buttons/SecondaryButton";
 import CartPopUp from "../cart/CartPopUp";
+import { sendGTMEvent } from "@next/third-parties/google";
 
 interface ProductCardProps {
   product: ProductItem;
@@ -69,6 +70,7 @@ export default function ProductCard({
       code: coloropts[selectedColorIndex]?.code,
       quantity: 1,
     });
+    sendGTMEvent({ event: "add_to_cart" });
     openModal(
       "cartPopUp",
       <CartPopUp shownOnAddonsProducts={shownOnAddonsProducts} />,
