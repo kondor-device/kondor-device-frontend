18fb2e8f9a1012bf3695be00a739223755118efe
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-06-26 12:58:13 +0300

    fixed header

--- src/components/notFoundPage/NotFoundPage.tsx
+++ src/components/notFoundPage/NotFoundPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PageTitle from "../shared/titles/PageTitle";
 import Button from "../shared/buttons/Button";
 import { useLocale, useTranslations } from "next-intl";
@@ -9,13 +8,13 @@ export default function NotFoundPage() {
   const locale = useLocale();
 
   return (
-    <>
+    <div className="pt-[82px] tabxl:pt-[113px]">
       <PageTitle>{t("notFoundPage.info")}</PageTitle>
       <section className="container max-w-[1920px] py-[100px]">
         <Link href="/" locale={locale} className="block w-fit mx-auto">
           <Button>{t("buttons.goHome")}</Button>
         </Link>
       </section>
-    </>
+    </div>
   );
 }
--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+"use client";
+import { usePathname } from "@/i18n/routing";
+import { useLocale } from "next-intl";
 import HeaderDesk from "./HeaderDesk";
 import HeaderMob from "./headerMob/HeaderMob";
 
 export default function Header() {
+  const locale = useLocale();
+  const pathname = usePathname();
+  const isHomePage = pathname === `/${locale}` || pathname === "/";
+
   return (
-    <header>
+    <header
+      className={`w-[100dvw] ${!isHomePage ? "fixed z-50 top-0 left-0" : ""}`}
+    >
       <HeaderDesk />
       <HeaderMob />
     </header>
