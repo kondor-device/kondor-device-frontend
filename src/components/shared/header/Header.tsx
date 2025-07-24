d155e2b2b5b6e20e1f5218e6316de6d8be7affed
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-07-24 23:37:54 +0300

    fixed header

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,7 +1,5 @@
 "use client";
 import { useState } from "react";
-import { usePathname } from "@/i18n/routing";
-import { useLocale } from "next-intl";
 import HeaderDesk from "./HeaderDesk";
 import HeaderMob from "./headerMob/HeaderMob";
 import CatalogMenu from "./catalogMenu/CatalogMenu";
@@ -14,17 +12,10 @@ interface HeaderProps {
 
 export default function Header({ categories }: HeaderProps) {
   const [isCatalogMenuOpened, setIsCatalogMenuOpened] = useState(false);
-  const locale = useLocale();
-  const pathname = usePathname();
-  const isHomePage = pathname === `/${locale}` || pathname === "/";
 
   return (
     <>
-      <header
-        className={`w-[100dvw] ${
-          !isHomePage ? "fixed z-[60] top-0 left-0" : ""
-        }`}
-      >
+      <header className={`w-[100dvw] fixed z-[60] top-0 left-0"`}>
         <HeaderDesk setIsCatalogMenuOpened={setIsCatalogMenuOpened} />
         <HeaderMob
           setIsCatalogMenuOpened={setIsCatalogMenuOpened}
