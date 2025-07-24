dc32d653f8f7a8beeb49f3c57ae96d639f6c8cb5
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-07-24 22:55:09 +0300

    added id

--- src/components/productPage/productInfo/ImagePicker.tsx
+++ src/components/productPage/productInfo/ImagePicker.tsx
@@ -44,7 +44,10 @@ export default function ImagePicker({ photos }: ImagePickerProps) {
 
   return (
     <>
-      <div className="gallery-container w-full max-w-[380px] tab:max-w-[514px] tabxl:max-w-[617px] tabxl:w-[calc(50%-40px)] desk:w-[calc(50%-60px)] mx-auto tabxl:mx-0 mb-8 tabxl:mb-0 scroll-mt-[142px] tabxl:scroll-mt-[173px]">
+      <div
+        id="all"
+        className="gallery-container w-full max-w-[380px] tab:max-w-[514px] tabxl:max-w-[617px] tabxl:w-[calc(50%-40px)] desk:w-[calc(50%-60px)] mx-auto tabxl:mx-0 mb-8 tabxl:mb-0 scroll-mt-[142px] tabxl:scroll-mt-[173px]"
+      >
         <ImageGallery
           ref={(ref) => {
             galleryRef.current = ref;
